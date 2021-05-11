40101cc5ecc349539e6f88e10f53cb916945075b
nomura <nomura@nomuranoMacBook-Air.local>
2021-05-11 12:16:53 +0900

    update_20210511

--- update_github.sh
+++ update_github.sh
@@ -4,20 +4,20 @@ find . -name '.DS_Store' -type f -ls -delete
 python freeze.py
 mv build docs
 
-# today=$(date "+%Y%m%d")
-# echo "update-heroku"
-# echo "git"
+today=$(date "+%Y%m%d")
+echo "update-heroku"
+echo "git"
+git add .
+git commit -m "update_${today}"
+# git remote add origin git@github.com:NomuraS/NomuraS.github.io.git
+git push -u origin main
+
+
+
+# git init
 # git add .
-# git commit -m "update_${today}"
+# git commit -m "modified for pages"
+# git branch -M main
 # git remote add origin git@github.com:NomuraS/NomuraS.github.io.git
 # git push -u origin main
-
-
-
-git init
-git add .
-git commit -m "modified for pages"
-git branch -M main
-git remote add origin git@github.com:NomuraS/NomuraS.github.io.git
-git push -u origin main
 
